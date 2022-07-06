663951239bcf315a10a0c47ada8f9f709104cbb5
Novazembla <nova@pro-pro.local>
2022-07-06 07:20:07 +0200

    bug fix: async/await in /api/revalidate

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,4 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { appConfig } from "~/config";
+
+const maybeLogError = async (error: any, alwaysSend?: boolean) => {
+  if (appConfig.errorLogUrl.trim() || alwaysSend) {
+    await fetch(appConfig.errorLogUrl, {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "",
+        message: "",
+        stack: "",
+        url: "",
+        navigator: "",
+        detectIt: "",
+        ...error,
+      }),
+    });
+  }
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -17,28 +39,62 @@ export default async function handler(
   try {
     // Check for secret to confirm this is a valid request
     if (req?.body?.token !== process.env.REVALIDATE_TOKEN) {
+      await maybeLogError(
+        {
+          name: "Error: /api/revalidate 401 Invalid token",
+        },
+        true
+      );
       return res.json({ status: 401, message: "Invalid token" });
     }
 
     if (req?.body?.paths?.length) {
-      req?.body?.paths?.forEach(async (path: string) => {
+      // !!! we use for (... of ...) loop as this handles async/await correctly
+      for (const path of req?.body?.paths) {
         if (path.trim()) {
-          let cleanPath = path;
+          let cleanPath = path.trim();
           // clean path from trailing slash if needed
           if (cleanPath !== "/")
             cleanPath = path.endsWith("/") ? path.slice(0, -1) : path;
 
-          await res.revalidate(cleanPath).catch((err) => {
-            // fail silently ... 
+          await maybeLogError({
+            name: `INFO: /api/revalidate trigger: ${cleanPath}`,
+          });
+
+          await res.revalidate(cleanPath).catch(async (error: any) => {
+            await maybeLogError(
+              {
+                name: `Error: /api/revalidate ${error?.name}`,
+                message: error?.message,
+                stack: error?.stack,
+                url: cleanPath,
+              },
+              true
+            );
           });
         }
+        // })
+      }
+      return res.json({ status: 200, message: "ok" });
+    } else {
+      await maybeLogError({
+        name: "Error: /api/revalidate 406 Please send an paths array with values",
+      });
+      res.json({
+        status: 406,
+        message: "Please send an paths array with values",
       });
     }
+  } catch (error: any) {
+    await maybeLogError(
+      {
+        name: `Error: /api/revalidate ${error?.name}`,
+        message: error?.message,
+        stack: error?.stack,
+      },
+      true
+    );
 
-    return res.json({ status: 200, message: "ok" });
-  } catch (err) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
     return res.send({ status: 500, message: "Error revalidating" });
   }
 }
