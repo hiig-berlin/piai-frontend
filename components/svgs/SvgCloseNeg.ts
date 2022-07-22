1da93bdb9dd01252a0c97736e7e6308eaeff3da8
Novazembla <nova@pro-pro.local>
2022-07-22 16:20:33 +0200

    change svg genarating script to properly type SVGs

--- components/svgs/SvgCloseNeg.ts
+++ components/svgs/SvgCloseNeg.ts
@@ -1,2 +1,2 @@
-export const SvgCloseNeg = '%3Csvg%20xmlns%3D"http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg"%20width%3D"76.296"%20height%3D"50.296"%3E%3Cpath%20fill%3D"none"%20stroke%3D"%23fff"%20stroke-linecap%3D"round"%20stroke-width%3D"6"%20d%3D"m14.148%2010.914%2032.181%2032.181M14.148%2043.381%2046.329%2011.2"%2F%3E%3C%2Fsvg%3E';
+export const SvgCloseNeg: string = '%3Csvg%20xmlns%3D"http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg"%20width%3D"76.296"%20height%3D"50.296"%3E%3Cpath%20fill%3D"none"%20stroke%3D"%23fff"%20stroke-linecap%3D"round"%20stroke-width%3D"6"%20d%3D"m14.148%2010.914%2032.181%2032.181M14.148%2043.381%2046.329%2011.2"%2F%3E%3C%2Fsvg%3E';
 export default SvgCloseNeg;
--- components/svgs/SvgRightArrow.ts
+++ components/svgs/SvgRightArrow.ts
@@ -1,2 +1,2 @@
-export const SvgRightArrow = '%3Csvg%20xmlns%3D"http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg"%20viewBox%3D"0%200%2033%2030"%3E%3Cpath%20d%3D"M18.09%2029.83%2033%2015%2018.11.16h-3.26l13.67%2013.55H0v2.52h28.52l-13.68%2013.6Z"%2F%3E%3C%2Fsvg%3E';
+export const SvgRightArrow: string = '%3Csvg%20xmlns%3D"http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg"%20viewBox%3D"0%200%2033%2030"%3E%3Cpath%20d%3D"M18.09%2029.83%2033%2015%2018.11.16h-3.26l13.67%2013.55H0v2.52h28.52l-13.68%2013.6Z"%2F%3E%3C%2Fsvg%3E';
 export default SvgRightArrow;
