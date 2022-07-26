265214cbe88e500c2dc4d06ac413062d68ff7d3b
Novazembla <nova@pro-pro.local>
2022-07-26 14:12:48 +0200

    better tool layout

--- components/layouts/LayoutTool.tsx
+++ components/layouts/LayoutTool.tsx
@@ -1,13 +1,26 @@
 import NextHeadSeo from "next-head-seo";
 import { useConfigContext } from "~/providers/ConfigContextProvider";
-import { AppProps } from "~/types";
 import { Menu } from "../app/Menu";
 import { UserTracking } from "../app/UserTracking";
 import { LoadingBar } from "../styled/LoadingBar";
 import { usePageStateContext } from "~/providers/PageStateContextProvider";
 import { MenuButton } from "../app/MenuButton";
-import { ToolSidebar } from "../app/ToolSidebar";
 import { Sidebar } from "../tools/shared/Sidebar";
+import styled from "styled-components";
+
+const ToolContainer = styled.div`
+  display: flex;
+  top: 0;
+  left: 0;
+  width: 100%;
+  min-height: 100%;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+
+  padding: var(--size-3);
+`;
 
 export const LayoutTool = ({
   children,
@@ -34,9 +47,13 @@ export const LayoutTool = ({
       />
       <UserTracking />
       <LoadingBar isLoading={isLoading} />
-      <Sidebar tool={props.tool.slug} />
+
       <MenuButton />
-      {children}
+
+      <ToolContainer>
+        <Sidebar tool={props.tool.slug} />
+        <ContentContainer>{children}</ContentContainer>
+      </ToolContainer>
       <Menu />
     </>
   );
