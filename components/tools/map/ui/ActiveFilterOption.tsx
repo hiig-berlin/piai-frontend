384a5d8aabb31d72e18d5f11064fcdcd820c5af8
Novazembla <nova@pro-pro.local>
2022-08-03 12:36:22 +0200

    trim active options label to one line

--- components/tools/map/ui/ActiveFilterOption.tsx
+++ components/tools/map/ui/ActiveFilterOption.tsx
@@ -13,7 +13,18 @@ const Option = styled.div`
   border: 1px solid #fff;
   padding: 0.3em 0 0.5em 0.5em;
   gap: var(--size-1);
+  max-width:100% ;
 `;
+
+const Trim = styled.span`
+  display: inline-block;
+  overflow: hidden;
+  // hyphens: auto;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  max-width: 100%;
+  
+`
 
 export const ActiveFilterOption = ({
   label,
@@ -24,7 +35,7 @@ export const ActiveFilterOption = ({
 }) => {
   return (
     <Option>
-      {safeHtml(label)}
+      <Trim>{safeHtml(label)}</Trim>
       <Icon type="close" onClick={onRemove} />
     </Option>
   );
