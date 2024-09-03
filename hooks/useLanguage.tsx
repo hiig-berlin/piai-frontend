66fa95077aff146e4c74ffb1e7978ec6590f64f8
crosssenses <lari_wu@gmx.de>
2024-09-03 12:37:17 +0200

    Add language to local storage to persist on menu/tool change

--- hooks/useLanguage.tsx
+++ hooks/useLanguage.tsx
@@ -23,9 +23,19 @@ const useLanguage = (toolSlug: string) => {
     if (typeof window !== "undefined") {
       const urlParams = new URLSearchParams(window.location.search);
       const urlLang = urlParams.get("lang");
+
+      let initialLang = "en"; // default language
+
       if (urlLang) {
-        setLanguage(urlLang);
+        initialLang = urlLang;
+      } else {
+        const storedLang = localStorage.getItem("selectedLang");
+        if (storedLang) {
+          initialLang = storedLang;
+        }
       }
+
+      setLanguage(initialLang);
     }
   }, []);
 
@@ -46,6 +56,7 @@ const useLanguage = (toolSlug: string) => {
 
     if (!isInitialLoad) {
       window.history.replaceState({}, "", `?lang=${language}`);
+      localStorage.setItem("selectedLang", language); // Update localStorage when language changes
     }
     setIsInitialLoad(false);
   }, [language, toolSlug, isInitialLoad]);
