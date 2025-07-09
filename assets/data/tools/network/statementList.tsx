d2f579482f1068601c02827e80195e92c5ccea9b
crosssenses <lari_wu@gmx.de>
2025-07-09 14:36:16 +0200

    Quote Wolfgang

--- assets/data/tools/network/statementList.tsx
+++ assets/data/tools/network/statementList.tsx
@@ -3,7 +3,7 @@ export const statementList = [
     author: "Prof. Dr. Wolfgang Schulz",
     affiliation: "Leibniz Institute for Media Research | Hans-Bredow-Institut",
     image: "/img/tools/network/wolfgang-schulz.jpg",
-    statement: "The network of PIAI is an important undertaking. Lorem ipsum dolor sit amet, consectetur adipiscing. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    statement: "The Public Interest AI Research Network is a great initiative to exchange global perspectives. Learning from each other and discussing how AI can support public interest goals, is an important alternative to build and demand. We hope the network can also foster synchronized efforts and create a stronger voice for societal perspectives on AI."
   },
   {
     author: "Lorem ipsum dolor sit amet",
