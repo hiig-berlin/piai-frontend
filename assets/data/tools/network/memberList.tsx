a8329bee5828be5c6e1def37c44e019cb6d65584
git-s23 <syoussef@systemli.org>
2025-10-06 09:39:17 +0200

    updated memberList

--- assets/data/tools/network/memberList.tsx
+++ assets/data/tools/network/memberList.tsx
@@ -38,26 +38,92 @@ export const memberList = [
     people: [
       {
         name: "Karen Boers",
-        role: "Managing director of FARI - AI for the Common Good Institute",
-        link: "https://www.fari.brussels",
+        role: "Managing Director of FARI - AI for the Common Good Institute",
+        link: "https://www.fari.brussels/community",
       },
       {
         name: "Dr. Carl Mörch",
+        role: "Co-Director of FARI - AI for the Common Good Institute",
+        link: "https://www.fari.brussels/community"
       },
     ],
   },
   {
-    name: "Do you want to join?",
+    name: "MI4People gGmbH",
     tags: ["Member"],
-    link: "https://publicinterest.ai/tool/network/join",
-    location: "Fill out our short application form.",
+    link: "https://www.mi4people.org/",
+    location: "Munich, Germany",
     people: [
       {
-        name: "Your name",
-        role: "Your role",
-        link: "https://www.your-website.com",
+        name: "Dr. Paul Springer",
+        role: "Co-Founder and Managing Director at MI4People",
+        link: "https://www.mi4people.org/paul-springer",
       },
-      
+    ],
+  },
+  {
+    name: "University of São Paulo",
+    tags: ["Member"],
+    link: "https://ciaam.usp.br/en/",
+    location: "São Paulo, Brazil",
+    people: [
+      {
+        name: "André Carlos Ponce de Leon F. de Carvalho",
+        role: "Full Professor and Director of the Institute of Mathematics and Computer Science (ICMC-USP)",
+        link: "https://ciaam.usp.br/en/our-team/",
+      },
+    ],
+  },
+  {
+    name: "Mila - Institut québécois d'intelligence artificielle",
+    tags: ["Member"],
+    link: "https://mila.quebec/en",
+    location: "Montréal, Canada",
+    people: [
+      {
+        name: "Benjamin Prud'homme",
+        role: "Benjamin Prud'homme is Vice-President of Policy, Safety and Global Affairs",
+        link: "https://mila.quebec/en/directory/benjamin-prudhomme",
+      },
+    ],
+  },
+  {
+    name: "Transformations Community",
+    tags: ["Member"],
+    link: "https://transformationscommunity.org/",
+    location: "Boulder, USA",
+    people: [
+      {
+        name: "Bruce Goldstein",
+        role: "Research Professor at the Institute of Behavioral Science at the University of Colorado Boulder",
+        link: "https://transformationscommunity.org/team/",
+      },
+    ],
+  },
+  {
+    name: "Pontificia Universidad Catolica de Chile",
+    tags: ["Member"],
+    link: "https://www.uc.cl/",
+    location: "Santiago, Chile",
+    people: [
+      {
+        name: "Gabriela Arriagada-Bruneau",
+        role: "Assistant Professor at the Institute of Applied Ethics and the Institute of Computational and Mathematical Engineering",
+        link: "https://eticasaplicadas.uc.cl/profesores/gabriela-arriagada-bruneau/",
+      },
+    ],
+  },
+  {
+    name: "National Centre for Artificial Intelligence (CENIA)",
+    tags: ["Member"],
+    link: "https://cenia.cl/",
+    location: "Macul, Chile",
+    people: [
+      {
+        name: "Gabriela Arriagada-Bruneau",
+        role: "Young Researcher",
+        link: "https://cenia.cl/en/profiles/?ref=gabriela-arriagada-bruneau",
+      },
     ],
   },
 ];
